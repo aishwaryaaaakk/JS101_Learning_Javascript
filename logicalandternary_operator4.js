14cebf25680a221593c277466ef094a1a94e328e
Aishwarya Kadam <aishwaryakadam0711@gmail.com>
2023-08-30 01:14:27 +0530

    Code for Logical & Ternary Operator 4

---
+++ logicalandternary_operator4.js
@@ -0,0 +1,17 @@
+//Given 3 numbers (all different values), print which is greatest.
+let a=50;
+let b=60;
+let c=70;
+
+if(a>b && a>c)
+{
+  console.log("A is the Greatest.");
+}
+else if(b>a && b>c)
+{
+  console.log("B is the Greatest.");
+}
+else
+{
+  console.log("C is the Greatest.");
+}
