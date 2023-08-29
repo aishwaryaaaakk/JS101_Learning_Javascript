d26b3f3260a1581accbe39a8c4860bbd6e67efb5
Aishwarya Kadam <aishwaryakadam0711@gmail.com>
2023-08-30 01:02:29 +0530

    Code for Conditional Statement 2

---
+++ conditional_statement2.js
@@ -0,0 +1,11 @@
+//If a person is allowed to drive in India print "Apply for a license" or "NA".
+let age=17;
+
+  if(age>=18)
+  {
+    console.log("Apply for license");
+  }
+  else
+  {
+   console.log("NA");
+  }
