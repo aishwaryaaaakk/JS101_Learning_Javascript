b06a4080cddbe371ecf0e3820f5f1de9d06ede0e
Aishwarya Kadam <aishwaryakadam0711@gmail.com>
2023-08-30 01:03:59 +0530

    Code for Conditional Statement 3

---
+++ conditional_statement3.js
@@ -0,0 +1,16 @@
+//Given 2 numbers a and b print which is greater or "both equal".
+let a=5;
+let b=2;
+
+if(a>b)
+{
+  console.log("a is greater");
+}
+else if(a<b)
+{
+  console.log("b is greater");
+}
+else
+{
+  console.log("both are equal"); 
+}
