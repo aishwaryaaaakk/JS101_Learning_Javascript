c8aaaecb9818b1d2a5e0d4df11835b1d0ad3acc3
Aishwarya Kadam <aishwaryakadam0711@gmail.com>
2023-08-30 01:12:59 +0530

    Code for Logical & Ternary Operator 3

---
+++ logicalandternary_operator3.js
@@ -0,0 +1,10 @@
+//Given and character if it is a consonant print "Consonant".
+let str="v";
+if(str=="a" || str=="e" || str=="i" || str=="o" || str=="u")
+{
+  console.log("Not a Consonant");
+}
+else
+{
+  console.log("Consonant");
+}
