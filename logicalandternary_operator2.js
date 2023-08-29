4b4daf5de0e13c54a90ed29f9631b070b230070f
Aishwarya Kadam <aishwaryakadam0711@gmail.com>
2023-08-30 01:11:24 +0530

    Code for Logical & Ternary Operator 2

---
+++ logicalandternary_operator2.js
@@ -0,0 +1,10 @@
+//Given any character, if it is a vowel print "Vowel".
+let str="a";
+if(str=="a" || str=="e" || str=="i" || str=="o" || str=="u")
+{
+  console.log("Vowel");
+}
+else
+{
+  console.log("Not a Vowel");
+}
