77e31e9090b08f53404537625b2e900bcbf32e6e
Aishwarya Kadam <aishwaryakadam0711@gmail.com>
2023-08-30 00:43:28 +0530

    Code for problem 4

---
+++ problem4.js
@@ -0,0 +1,18 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+console.log("*********RESULT**********")
+var name="NAME:- Bheem";
+console.log(name);
+let school="SCHOOL:- Dholakpur School"
+console.log(school)
+var grade="GARDE:- A+";
+  console.log(grade)
+var rollno="ROLL NO.:- 17";
+console.log(rollno)
+console.log("******👍RESULT👌******")
+
+var sub="STRENGTH        POTENTIAL         SMART";
+var marks="99%               92%              97.2%";
+console.log(sub)
+console.log(marks)
